7460b84fb71374116a83aa086cefae733225d8fa
felipemaxplay <felipemaxplay@gmail.com>
2023-02-04 14:59:08 -0300

    annotation repository

--- src/main/java/br/com/felipemaxplay/digitalacademic/repository/PhysicalAssessmentRepository.java
+++ src/main/java/br/com/felipemaxplay/digitalacademic/repository/PhysicalAssessmentRepository.java
@@ -2,6 +2,8 @@ package br.com.felipemaxplay.digitalacademic.repository;
 
 import br.com.felipemaxplay.digitalacademic.entity.PhysicalAssessment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PhysicalAssessmentRepository extends JpaRepository<PhysicalAssessment, Long> {
 }
--- src/main/java/br/com/felipemaxplay/digitalacademic/repository/TuitionRepository.java
+++ src/main/java/br/com/felipemaxplay/digitalacademic/repository/TuitionRepository.java
@@ -2,6 +2,8 @@ package br.com.felipemaxplay.digitalacademic.repository;
 
 import br.com.felipemaxplay.digitalacademic.entity.Tuition;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TuitionRepository extends JpaRepository<Tuition, Long> {
 }
