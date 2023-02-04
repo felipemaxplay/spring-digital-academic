55b0ae78c059fb3b1eb3898a11bcf25c5d1c5023
felipemaxplay <felipemaxplay@gmail.com>
2023-02-04 11:30:31 -0300

    init

---
+++ src/main/java/br/com/felipemaxplay/digitalacademic/DigitalAcademicApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipemaxplay.digitalacademic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalAcademicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalAcademicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/felipemaxplay/digitalacademic/DigitalAcademicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipemaxplay.digitalacademic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalAcademicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
