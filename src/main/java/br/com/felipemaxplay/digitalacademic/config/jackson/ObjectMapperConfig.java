afe555e32b9088b3f5874c6db4f61defd2ea4c6a
felipemaxplay <felipemaxplay@gmail.com>
2023-02-04 16:35:16 -0300

    CONFIGURATION

--- src/main/java/br/com/felipemaxplay/digitalacademic/config/jackson/ObjectMapperConfig.java
+++ src/main/java/br/com/felipemaxplay/digitalacademic/config/jackson/ObjectMapperConfig.java
@@ -6,16 +6,18 @@ import br.com.felipemaxplay.digitalacademic.config.jackson.serializer.LocalDateS
 import br.com.felipemaxplay.digitalacademic.config.jackson.serializer.LocalDateTimeSerializer;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Configuration
 public class ObjectMapperConfig implements Jackson2ObjectMapperBuilderCustomizer {
 
     @Override
-    public void customize(Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder) {
-        jackson2ObjectMapperBuilder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+    public void customize(Jackson2ObjectMapperBuilder jacksonObjectMapperBuilder) {
+        jacksonObjectMapperBuilder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                 .serializerByType(LocalDate.class, new LocalDateSerializer())
                 .serializerByType(LocalDateTime.class, new LocalDateTimeSerializer())
                 .deserializerByType(LocalDate.class, new LocalDateDeserializer())
